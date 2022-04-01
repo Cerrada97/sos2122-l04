e7ef856b840d12d474cd7bf3e943799c5dd13d37
Cerrada97 <cerradabe@gmail.com>
2022-04-01 11:11:02 +0200

    changes

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-
-/*console.log(cool());*/
-
 const express = require("express");
+const backend = required("./src/back");
 
-const app = express();
 const port = process.env.PORT || 8081;  /*servidor para abrirlo en un entorno (hay que definir una variable de entorno) y si no la uso en un entorno pues abre el 8081 en navegador*/
 
-app.use("/",express.static('public'));
-
-app.get("/api", (req,res)=>{
-    console.log("Requested / route");
-    res.send({result:true});
-});
-/**/ 
-app.get("/time", (req,res)=>{
-    console.log("Requested / route");
-    res.send("<html><body><h1>"+new Date()+"</h1></body></html>")
-});
+const app = express();
+backend(app);
 
 app.listen(port, () => {
-
-console.log(`Server ready at port ${port}`);
+    console.log(`Server ready at port ${port}`);
 });
 
 
---
+++ src/back/index.js
@@ -0,0 +1,7 @@
+module.exports = (app) =>{
+    
+    app.get("/api", (req,res)=>{
+        console.log("Requested / route");
+        res.send({result:true});
+    });
+}
